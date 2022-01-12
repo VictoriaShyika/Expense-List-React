1851df2c2c1810189c86720c4539315b811b2a16
VictoriaShyika <victoriashyika@gmail.com>
2022-01-12 23:29:24 +0200

    Added new Components and Updated Code

--- src/App.js
+++ src/App.js
@@ -1,56 +1,36 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
-
-  const expense = [
+  const expenses = [
     {
-      id: 'e1',
-      title: 'Apple',
-      amount: 6.10,
+      id: "e1",
+      title: "Apple",
+      amount: 6.1,
       date: new Date(2020, 7, 14),
     },
     {
-      id: 'e2',
-      title: 'Lemon',
+      id: "e2",
+      title: "Lemon",
       amount: 2.45,
       date: new Date(2021, 2, 12),
     },
     {
-      id: 'e3',
-      title: 'Potato',
-      amount: 4.20,
+      id: "e3",
+      title: "Potato",
+      amount: 4.2,
       date: new Date(2020, 2, 28),
     },
     {
-      id: 'e4',
-      title: 'Grape',
-      amount: 10.80,
+      id: "e4",
+      title: "Grape",
+      amount: 10.8,
       date: new Date(2021, 5, 12),
     },
-  ]
+  ];
   return (
-    <div className="App">
-      <ExpenseItem 
-      title={expense[0].title} 
-      amount={expense[0].amount} 
-      date={expense[0].date}
-     ></ExpenseItem> 
-           <ExpenseItem 
-      title={expense[1].title} 
-      amount={expense[1].amount} 
-      date={expense[1].date}
-     ></ExpenseItem> 
-           <ExpenseItem 
-      title={expense[2].title} 
-      amount={expense[2].amount} 
-      date={expense[2].date}
-     ></ExpenseItem> 
-           <ExpenseItem 
-      title={expense[3].title} 
-      amount={expense[3].amount} 
-      date={expense[3].date}
-     ></ExpenseItem> 
-
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
